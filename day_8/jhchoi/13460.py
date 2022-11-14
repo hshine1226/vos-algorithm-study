18a3201f38a736982c6135c5ccd4ee853948d89c
hshine1226 <hshine1226@gmail.com>
2022-11-14 23:22:04 +0900

    Update 13460

--- day_8/jhchoi/13460.py
+++ day_8/jhchoi/13460.py
@@ -4,13 +4,18 @@ from collections import deque
 def input():
     return sys.stdin.readline().rstrip()
 
+# 빨간 구슬을 구멍으로 나오면 통과
+# 파란구슬이 구멍에 빠지면 실패
+# 구슬은 사방탐색 가능 (구슬은 동시에 움직임)
+# 빨간 구슬과 파란 구슬은 같은 칸에 있을 수 없음
+# 움직인 횟수가 10회를 초과하면 실패
+
 n, m = map(int, input().split())
 
 board = []
 [board.append(list(input())) for _ in range(n)]
 
 direction = [(-1,0), (1,0), (0,1), (0,-1)]
-visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
 
 def move(x, y, dx, dy):
     cnt = 0
@@ -23,6 +28,9 @@ def move(x, y, dx, dy):
 def bfs(rx, ry, bx, by): 
     need_visit = deque()
     need_visit.append((1, rx, ry, bx, by))
+    # [rx][ry][bx][by]
+    visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+
     while need_visit:
         cnt, rx, ry, bx, by = need_visit.popleft()
         visited[rx][ry][bx][by] = True
@@ -31,23 +39,23 @@ def bfs(rx, ry, bx, by):
             return -1
 
         for dx, dy in direction:
-            next_rx, next_ry, next_rcnt = move(rx, ry, dx, dy)
-            next_bx, next_by, next_bcnt = move(bx, by, dx, dy)
+            rx_next, ry_next, rcnt_next = move(rx, ry, dx, dy)
+            bx_next, by_next, bcnt_next = move(bx, by, dx, dy)
 
-            if board[next_bx][next_by] != 'O':
-                if board[next_rx][next_ry] == 'O':
+            if board[bx_next][by_next] != 'O':
+                if board[rx_next][ry_next] == 'O':
                     return cnt
-                if next_rx == next_bx and next_ry == next_by:
-                    if next_rcnt > next_bcnt:
-                        next_rx -= dx
-                        next_ry -= dy
+                if rx_next == bx_next and ry_next == by_next:
+                    if rcnt_next > bcnt_next:
+                        rx_next -= dx
+                        ry_next -= dy
                     else:
-                        next_bx -= dx
-                        next_by -= dy
+                        bx_next -= dx
+                        by_next -= dy
             
-            if not visited[next_rx][next_ry][next_bx][next_by]:
-                visited[next_rx][next_ry][next_bx][next_by] = True
-                need_visit.append((cnt + 1, next_rx, next_ry, next_bx, next_by))
+            if not visited[rx_next][ry_next][bx_next][by_next]:
+                visited[rx_next][ry_next][bx_next][by_next] = True
+                need_visit.append((cnt + 1, rx_next, ry_next, bx_next, by_next))
     return -1
 
 rx, ry, bx, by = 0, 0, 0, 0
